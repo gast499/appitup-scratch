150e750a47bf1009ec9d847406bd9ce615cb1cbd
copo2496 <copo876@gmail.com>
2018-04-18 19:03:27 -0400

    Having 2 pages for signup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,62 +7,65 @@
 <div class="row" id="whoareyou">
     Who are you?
 </div>
-<form method="POST" action="{{ route('register') }}">
+<form method="POST" id="signupForm" action="{{ route('register') }}">
     @csrf
-    <div class="form-group row">
-        <div class="card card-select" id="data-cardSelect1" data-value="Dreamer" onclick="selectCard('1')">
-            <img class="card-img-top signup-img" src="{{asset('assets/images/dreamer.png')}}">
-            <div class="card-body">
-                <h5 class="card-title">Dreamer</h5>
+    <div id="selectType">
+        <div class="form-group row">
+            <div class="card card-select" id="data-cardSelect1" data-value="Dreamer" onclick="selectCard('1')">
+                <img class="card-img-top signup-img" src="{{asset('assets/images/dreamer.png')}}">
+                <div class="card-body">
+                    <h5 class="card-title">Dreamer</h5>
+                </div>
+            </div>
+            <div class="card card-select" id="data-cardSelect2" data-value="Creator" onclick="selectCard('2')">
+                <img class="card-img-top signup-img" src="{{asset('assets/images/creator.png')}}">
+                <div class="card-body">
+                    <h5 class="card-title">Creator</h5>
+                </div>
+            </div>
+            <input type="hidden" id="type" name="type" value="">;
+        </div>
+    </div>
+    <div id="emailpassword">
+        <div class="form-group row">
+            <label for="email"
+            class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+            <div class="col-md-6">
+                <input id="email" type="email"
+                class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                name="email" value="{{ old('email') }}" required>
+
+                @if ($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
             </div>
         </div>
-        <div class="card card-select" id="data-cardSelect2" data-value="Creator" onclick="selectCard('2')">
-            <img class="card-img-top signup-img" src="{{asset('assets/images/creator.png')}}">
-            <div class="card-body">
-                <h5 class="card-title">Creator</h5>
+        <div class="form-group row">
+            <label for="password"
+            class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+            <div class="col-md-6">
+                <input id="password" type="password"
+                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                name="password" required>
+
+                @if ($errors->has('password'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
             </div>
         </div>
-        <input type="hidden" id="type" name="type" value="">;
-    </div>
-    <div class="form-group row">
-        <label for="email"
-        class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-        <div class="col-md-6">
-            <input id="email" type="email"
-            class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-            name="email" value="{{ old('email') }}" required>
-
-            @if ($errors->has('email'))
-            <span class="invalid-feedback">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-            @endif
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label for="password"
-        class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-        <div class="col-md-6">
-            <input id="password" type="password"
-            class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-            name="password" required>
-
-            @if ($errors->has('password'))
-            <span class="invalid-feedback">
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-            @endif
-        </div>
-    </div>
-
-    <div class="form-group row mb-0">
-        <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Register') }}
-            </button>
+        <div class="form-group row mb-0">
+            <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Register') }}
+                </button>
+            </div>
         </div>
     </div>
 </form>

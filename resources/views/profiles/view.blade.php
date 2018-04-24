1cf1db0fc9d0dd729280d2a1b5715ecfb7c8a80e
jbo5 <jbo5@njit.edu>
2018-04-24 19:11:18 -0400

    testing changes again

--- resources/views/profiles/view.blade.php
+++ resources/views/profiles/view.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-		
+    <div class="container">		
 		
             <div class="personal-info">
 			
-			
+				<div class="row">
+					<div class="col-md-2">
+						<img src="{{\Illuminate\Support\Facades\Storage::url('public/avatars')}}/{{$user->id}}/{{ $user->avatar }}"
+                         class="img-rounded">
+					
+					</div>
+					<div class="col-md-4">
+					<h3>{{$user->first_name}} {{$user->last_name}}</h3>
+					<h3>2D Game Developer</h3>
+					<h3>52 projects completed</h3>
+					</div>
+					<div class="col-md-6">
+						<h3>Contact Me</h3>
+						<h3>Work with me</h3>					
+					</div>					
+				
+				</div>
 			
 			
                 <h3>{{$user->type}}'s Profile</h3>
@@ -194,6 +208,6 @@
 			
 			
 			
-        </div>
+        
     </div>
 @endsection

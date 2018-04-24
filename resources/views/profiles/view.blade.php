ff9d595bdbe4413bc836ec149538b4250d662ccd
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-24 19:19:03 -0400

    changed debug mode

--- resources/views/profiles/view.blade.php
+++ resources/views/profiles/view.blade.php
@@ -169,7 +169,7 @@
                         <div class="row col-md-9">
                             <h4>Categories</h4>
                             <br><br>
-                            {{dd($idea->categories)}}
+                            {{dd($idea->categories->count())}}
                             @foreach($idea->categories as $cat)
                                 <div class="card card-select" id="{{$cat->name}}-cardSelect" style="width: 18rem;"
                                      data-value="{{$cat->id}}">
